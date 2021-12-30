05dd4fb7ee6b0bfb1fcdb46308e594a2867dee46
Geca13 <karafiloski84@gmail.com>
2021-12-31 00:59:17 +0100

    changing css

--- src/main/java/com/example/erafmak/resourse/ArtistController.java
+++ src/main/java/com/example/erafmak/resourse/ArtistController.java
@@ -1,10 +1,14 @@
 package com.example.erafmak.resourse;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.erafmak.entity.Artist;
 
@@ -20,5 +24,10 @@ public class ArtistController {
 		return "redirect:/single/" + id; 
 		
 	}
-
+	
+	@PostMapping("/updateArtistImage/{id}/{aid}")
+	public String updateArtistImage(@PathVariable("id")Integer id, @PathVariable("aid")Integer aid,  @RequestParam("fileImage") MultipartFile multiPartFile) throws IOException {
+		artistService.updateImage(aid,multiPartFile);
+		return "redirect:/single/" + id; 
+	}
 }
--- src/main/java/com/example/erafmak/resourse/ArtistService.java
+++ src/main/java/com/example/erafmak/resourse/ArtistService.java
@@ -1,7 +1,11 @@
 package com.example.erafmak.resourse;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.erafmak.entity.Artist;
 import com.example.erafmak.entity.Song;
@@ -15,6 +19,9 @@ public class ArtistService {
 	
 	@Autowired
 	SongService songService;
+	
+	@Autowired
+	ImageService imageService;
 
 	public void createNewArtist(Artist artist, Integer sid) {
 		Song song = songService.findSongById(sid);
@@ -24,6 +31,15 @@ public class ArtistService {
 		artistRepository.save(newArtist);
 		song.getArtists().add(newArtist);
 		songService.updateSong(song);
+	}
+
+	public void updateImage(Integer aid, MultipartFile multiPartFile) throws IOException {
+		Artist artist = artistRepository.findById(aid).get();
+		String fileName = StringUtils.cleanPath(multiPartFile.getOriginalFilename());
+		artist.setImageUrl("/img/" + fileName);
+		imageService.uploadImage(multiPartFile);
+		artistRepository.save(artist);
+		
 	}
 	
 	
